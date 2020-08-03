84c24cd234de58049183e734279c7acd8b77088f
PeggyK1 <peggy.krom@protonmail.com>
2020-08-03 16:44:01 -0600

    Module 1 Commit

---
+++ Lambdata_peggyk1/__init__.py/vscode-report-null.py
@@ -0,0 +1,11 @@
+"""
+Function to Report NaN Values
+"""
+
+import pandas as pd
+import numpy as np
+
+def report_NaN(df):
+    results = df.isnull().sum()
+
+    print(results)
---
+++ Setup.py
@@ -0,0 +1,31 @@
+"""
+Lambdata - A collection of helpful data science functions
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy"
+    "pandas"
+]
+
+wiht open("README.md", "r") as file:
+    LONG_DESCRIPTION = file.read()
+
+setuptools.setup(
+    name="Lambdata_peggyk1",
+    version="0.0.1",
+    author="peggyk1",
+    description="A collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PeggyK1/Lambdata",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
